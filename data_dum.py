0ec28b4464efd07525bc53bc2d2e573b58a886dc
RakeshSahoo20 <rakesh.sahoo15@gmail.com>
2023-04-18 13:15:59 +0530

    updated requirements.txt

--- data_dum.py
+++ data_dum.py
@@ -33,3 +33,4 @@ client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
 
 
 
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="rakesh",
+    author_email="rakesh.sahoo15@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+
+)
